6cf7f629739a0af78e4f7fe36633e751eb1992d2
malindarivers <43344616+malindarivers@users.noreply.github.com>
2019-04-10 09:37:33 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$("button").click(function() {
+    
+    var inputOne = $(".input1").val();
+   
+    var inputTwo = $(".input2").val();
+    
+    var inputThree = $(".input3").val();
+
+ $("").text();
+    
+});
+    
